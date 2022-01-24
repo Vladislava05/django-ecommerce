a5f4d8b017acf48535f26a8a3fcc6abdc8174217
Vladislava Cherkasova <vladislavac74@gmail.com>
2022-01-24 10:05:10 +0300

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,16 @@
+new Vue({
+
+    delimiters: ['[[',']]'],
+    el: '#app',
+    data(){
+        return{
+            navs: [
+                {name: 'Store'},
+                {name: 'Orders'}
+
+            ],
+            
+        
+        }
+    }
+})
